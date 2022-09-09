8ad8f5e1e61c7445229018704f539b1f4487ff12
Farid Mansimli <f.mensimli@asan.gov.az>
2022-09-09 17:10:56 +0400

    conf:db and schema changes

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,5 +1,9 @@
 import { connect } from "mongoose";
 
 export const connectDb = async () => {
-  connect(process.env.MONGO_URL as string);
+  connect(process.env.MONGO_URL as string, {
+    autoCreate: false,
+    autoIndex: false,
+    minPoolSize: 5,
+  });
 };
